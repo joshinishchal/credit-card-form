f03363b1f36ab52ad47b394397ae50263edfdc06
Nishchal Joshi <njoshi@ten-x.com>
2018-03-31 19:16:39 -0700

    Creating container for CC form

---
+++ src/client/containers/creditCardForm/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CreditCardForm extends Component {
+  render () {
+    return (
+      <div>
+        Enter your credit card information
+      </div>
+    )
+  }
+}
+
+export default CreditCardForm
