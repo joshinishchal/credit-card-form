2f8fcfaeee971f956ca5250f8ddbbc38629b3eee
Nishchal Joshi <njoshi@ten-x.com>
2018-04-01 22:24:07 -0700

    Initia commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import styles from './App.scss'
+import cx from 'classnames/bind'
+const classNames = cx.bind(styles)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={classNames('App')}>
+        <header className={classNames('App-header')}>
+          <img src={logo} className={classNames('App-logo')} alt="logo" />
+          <h1 className={classNames('App-title')}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={classNames('App-intro')}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
